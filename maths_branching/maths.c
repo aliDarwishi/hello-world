91b912940c9fb434c4fc529107f8da95c6241b6f
Ali Baba Darwishi <P14136337@GWHXX583LIN016W.lec-admin.dmu.ac.uk>
2016-02-17 18:31:03 +0000

    conflict

---
+++ maths_branching/maths.c
@@ -0,0 +1,26 @@
+//This is the line causing issue in the conflict branch
+#include <stdio.h>
+
+int main()
+{	
+	int x = 0;
+	int add =0;
+	printf("X = %i", x)
+	
+	printf("Add something to X");
+	scanf(&add);
+	
+	x += add;
+	
+	
+	int a, b;
+	a = 10;
+	b = 5
+	
+	printf("%i % %i = %i", a, b, a%b);
+	
+	
+	
+	
+	
+}
